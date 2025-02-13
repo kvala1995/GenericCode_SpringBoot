81da008204febdfa569b4771c6431213d19b1ba7
kvala1995 <kvala@umich.edu>
2025-02-13 18:30:52 -0500

    Updated Practice_01_30_25

---
+++ src/main/java/com/Spring/Practice_01_30_25/Practice013025Application.java
@@ -0,0 +1,13 @@
+package com.Spring.Practice_01_30_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice013025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice013025Application.class, args);
+	}
+
+}
