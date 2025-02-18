7dc04c831f77eed2604e8b6ba0ccc2405cc773fd
kvala1995 <kvala@umich.edu>
2025-02-18 12:08:40 -0500

    Autowired Dependency

---
+++ src/main/java/com/Spring/Practice_01_30_25/Bean/BeanClass.java
@@ -0,0 +1,11 @@
+package com.Spring.Practice_01_30_25.Bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanClass {
+
+	public int sum(int a , int b) {
+		return a+b;
+	}
+}
--- src/main/java/com/Spring/Practice_01_30_25/Practice013025Application.java
+++ src/main/java/com/Spring/Practice_01_30_25/Practice013025Application.java
@@ -11,3 +11,4 @@ public class Practice013025Application {
 	}
 
 }
+
